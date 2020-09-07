1a83366dce10f8bf2308423c74a3b1ef9a418886
Aleksandr Stroev <simplesnake@mail.ru>
2020-09-07 21:52:24 +0300

    Initial Commit

---
+++ iOSDepartment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSDepartment
+//
+//  Created by Александр Строев on 07.09.2020.
+//  Copyright © 2020 Stroev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
