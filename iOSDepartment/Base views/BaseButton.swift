fc9aa4bb782ba1bad2b4b4199bf47be206408181
Aleksandr Stroev <simplesnake@icloud.com>
2022-03-06 11:58:43 +0300

    BaseButton refactor

--- iOSDepartment/Base views/BaseButton.swift
+++ iOSDepartment/Base views/BaseButton.swift
@@ -31,13 +31,61 @@ class BaseButton: UIButton {
         }
     }
     
+    var onLongTap: ((BaseButton) -> ())? {
+        didSet {
+            removeGestureRecognizer(longPressRecognizer)
+            addGestureRecognizer(longPressRecognizer)
+        }
+    }
+    
+    var onLongPressed: ((BaseButton) -> ())? {
+        didSet {
+            removeGestureRecognizer(longPressRecognizer)
+            addGestureRecognizer(longPressRecognizer)
+        }
+    }
+    
+    var longPressDuration: CGFloat = 0.5 {
+        didSet {
+            removeGestureRecognizer(longPressRecognizer)
+            addGestureRecognizer(longPressRecognizer)
+        }
+    }
+    
+    lazy var longPressRecognizer: UILongPressGestureRecognizer = {
+        let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(onLongPressedHandler))
+        longPressRecognizer.minimumPressDuration = Double(longPressDuration)
+        return longPressRecognizer
+    }()
+    
     @objc final func onTapHandler() {
         onTap?(self)
+    }
+    
+    @objc final func onLongTapHandler() {
+        onLongTap?(self)
+    }
+    
+    @objc final func onLongPressedHandler() {
+        if longPressRecognizer.state == .ended {
+            onLongTap?(self)
+        }
+        
+        if longPressRecognizer.state == .began {
+            onLongPressed?(self)
+        }
     }
     
     var text: String {
         set { setTitle(newValue, for: .normal) }
         get { return title(for: .normal) ?? "" }
     }
+    
+    init() {
+        super.init(frame: CGRect.zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
-
